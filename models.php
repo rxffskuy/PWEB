da1d52e5bdcbb226e42a226e97f4c2edf2396ec2
rxffskuy <146223904+rxffskuy@users.noreply.github.com>
2024-04-08 00:01:00 +0700

    Add files via upload

---
+++ models.php
@@ -0,0 +1,26 @@
+<?php
+
+class Contact {
+    protected static $conn;
+
+    public static function initialize() {
+      
+    }
+
+    public static function create() {
+        
+    }
+
+    public static function read() {
+        
+    }
+
+    public static function update() {
+       
+    }
+
+    public static function delete() {
+     
+    }
+}
+?>
